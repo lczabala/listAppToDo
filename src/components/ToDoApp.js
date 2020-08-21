1661b60677e903847376157be1c1847ced4532e3
Luis Zabala <lzabala28@gmail.com>
2020-08-21 11:25:35 -0400

    se agrega aopción para agregar tarea

--- src/components/ToDoApp.js
+++ src/components/ToDoApp.js
@@ -1,25 +1,88 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useEffect } from 'react';
 import {todoReducer} from './todoReducer';
-
-const initialState = [{
-    id: new Date().getTime(),
-    desc: 'Aprender React',
-    done: false
-}]
+import { useForms } from '../hooks/useForms';
 
 export const ToDoApp = () => {
 
-    const [todo] = useReducer(todoReducer, initialState)
+    const init = () =>{
+        return JSON.parse(localStorage.getItem('todo')) || [];
+    }
+
+    const [todo, dispatch] = useReducer(todoReducer, [], init);
+
+    //Tras llamar al useForm, re recibe el valor del input y la función para tomar el cambio del input
+    const [{taskDescription}, handleOnChange, reset] = useForms({
+        //Name que se colocó al input
+        taskDescription: ''
+    })
+
+    useEffect(() => {
+        localStorage.setItem('todo', JSON.stringify(todo));
+    }, [todo]);
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+
+        if (taskDescription.trim().length <= 1) {
+            return;
+        }
+
+        const newTask = {
+            id: new Date().getTime(),
+            desc: taskDescription,
+            done: false
+        }
+
+        //En el action se define el tipo de acción a realizar, en base a esto, en el reducer se evalúa ese tipo para realizar la actualización de estado
+        //En el action se envia tambien el nuevo dato (objeto) que se va a enviar al reducer para la actualización del estado
+        const action = {
+            type: 'add',
+            payload: newTask
+        }
+
+        //El dispatch nos permite disparar la acción al reducer
+        dispatch(action);
+
+        reset();
+    }
 
     return (
-        <div className="container-fluid">
+        <div className="container-fluid text-center">
             <h1>ToDo App</h1>
-
-            <div>
-                <ul>
-                    <li>Hola 1</li>
-                    <li>Hola 2</li>
-                    <li>Hola 3</li>
+            <div className="container addTask col-6">
+                <h5>Add ToDo</h5>
+                <div className="row justify-content-center">                    
+                    <div className="flex"> 
+                        <form onSubmit={handleSubmit}>
+                            <div className="input-group mb-3">                        
+                                <input 
+                                    type="text" 
+                                    className="form-control" 
+                                    placeholder="Task name"                                     
+                                    name= "taskDescription"
+                                    value= {taskDescription}
+                                    onChange= {handleOnChange}
+                                />
+                                <button type="submit" className="btn btn-primary ml-4 btn-sm">Add</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            
+            <div className="row justify-content-center">
+                <ul className="list-group col-6 padding-0">
+                    {
+                        todo.map((todo, i) => (
+                            <li
+                                key={todo.id}
+                                className="list-group-item flex"
+                            >
+                                <p>{i + 1} - {todo.desc}</p>
+                                <button className="btn btn-danger ml-4 btn-sm">Delete</button>
+                            </li>
+                        ))
+                    }
                 </ul>
             </div>
         </div>
--- src/components/todoReducer.js
+++ src/components/todoReducer.js
@@ -1,12 +1,10 @@
-const todoReducer = (state = [], action) =>{
+export const todoReducer = (state = [], action) =>{
 
     switch (action.type) {
-        //case 'add':
-            
-        //   break;
+        case 'add':            
+            return [...state, action.payload]
     
         default:
             return state;
     }
-
 }
